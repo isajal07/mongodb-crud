25746fd31280f72aaa0a9d2e4e52ef1ce7d89d86
isajal07 <isajal07@iSajal07-2.local>
2020-02-03 14:15:49 +0545

    Added Assoication test

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise; //Default promise from the library. 
+mongoose.Promise = global.Promise;
 
 before((done) => {
   mongoose.connect('mongodb://localhost/users_test');
   mongoose.connection
-  .once('open',()=> {done();})
-  .on('error',(error)=>{
-    console.warn('Warning', error);
-  });
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
 });
 
-beforeEach((done)=>{
-  mongoose.connection.collections.users.drop(()=>{
-    done();
+beforeEach((done) => {
+  const { users, comments, blogposts } = mongoose.connection.collections;
+  users.drop(() => {
+    comments.drop(() => {
+      blogposts.drop(() => {
+        done();
+      });
+    });
   });
 });
